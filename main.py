2f6ca9ce24ef7e8fc67a7ed7a9a071638430133b
gp2112 <guipaixao2000@protonmail.ch>
2017-06-21 17:01:49 -0300

    Adicionado novas opções e erro

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import telepot
-import requests
+from comandos import *
 
 bot = telepot.Bot("TOKEN")
 
@@ -8,34 +8,42 @@ print("Bot Iniciado!\n")
 
 def msg_tx(msg):
     print(msg)
-    msg1 = str(msg["text"])
-    cmd = msg1.split()
-    if msg["text"] == "/start":
-        bot.sendMessage(msg["chat"]["id"], "Olá! Seja bem-vindo ao Block Chain Bot beta!\n Você pode consultar transações, carteiras e blocos utilizando comandos.\nDigite /help para ver os comandos.\n\nDesenvolvido por @A4narchy")
-    elif msg["text"] == "/help":
-        bot.sendMessage(msg["chat"]["id"], "Comandos:\n /t <hash da transação>: ver informações de uma transação"
-                                                    "\n Outros em breve")
+    msg1 = str(msg["text"])     #texto enviado ao Bot
+    cmd = msg1.split()          #divide a string em lista
+    chat_id = msg["chat"]["id"]     #ID do chat
+
+    if msg["text"] == "/start":     #comando de iniciar Bot
+        bot.sendMessage(chat_id, "Olá! Seja bem-vindo ao Block Chain Bot beta!\n Você pode consultar transações, carteiras e blocos utilizando comandos.\nDigite /help para ver os comandos.\n\nDesenvolvido por @A4narchy")
+
+    elif msg["text"] == "/help": #comando para mostrar comandos disponíveis
+        bot.sendMessage(chat_id, "Comandos:\n /t <hash da transação>: ver informações de uma transação"
+                                                    "\n /w <enderesso da carteira>: ver informações de um endereçoo wallet"
+                                                    "\n /qr <enderesso>: gera QR code para endereço")
     elif cmd[0] == "/t":
+        hash = str(cmd[1])                      #hash será usado para definir as informações contidas no código. No caso o enderesso de transações e carteiras
+        try:
+            transaction(hash, chat_id, bot)
+        except:
+            bot.sendMessage(chat_id, emoji.emojize(" :red_circle: Erro no comando! Digite /help para ver o modelo.", use_aliases=True))
+
+
+    elif cmd[0] == "/w":
         hash = str(cmd[1])
-        info_r = requests.get("https://api.blockcypher.com/v1/btc/main/txs/%s" %hash)
-        info = info_r.json()
-        adress_in = info["addresses"][1]
-        adress_in1 = str(adress_in)
-        adress_out = info["addresses"][0]
-        adress_out1 = str(adress_out)
-        valor_r1 = info["total"]
-        valor_r2 = float(valor_r1)*0.00000001
-        fee1 = float(info["fees"])*0.00000001
-        valor_env =  valor_r2 + fee1
-        receiv = info["received"]
-        receiv1 = str(receiv)
-        conf = info["confirmations"]
-        conf1 = str(conf)
-        bot.sendMessage(msg["chat"]["id"], "Recebido em: %s\n\n Endereço de Saída: %s \n\n Endereço de Entrada: %s \n\n Valor enviado: %f btc\n\n Valor recebido: %f btc\n\n Taxa: %f btc\n\n  Confirmações: %s\n\n\n Informações por Blockcypher.com" %(receiv1, adress_in1, adress_out1, valor_env, valor_r2, fee1, conf))
+        try:
+            wallet(hash, chat_id, bot)          #funções disponíveis no arquivo "comandos.py"
+        except:
+            bot.sendMessage(chat_id, emoji.emojize(" :red_circle: Erro no comando! Digite /help para ver o modelo.", use_aliases=True))
+
+    elif cmd[0] == "/qr":
+        hash = str(cmd[1])
+        try:
+            qr_code(hash, bot, chat_id)
+        except:
+            bot.sendMessage(chat_id, emoji.emojize(" :red_circle: Erro no comando! Digite /help para ver o modelo.", use_aliases=True))
 
 bot.message_loop(msg_tx)
 
-while True:
+while True:         #Looping
     pass
 
     
