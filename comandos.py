3938214c0d72c3dc51443dcf1ad752e81cb92dc9
gp2112 <guipaixao2000@protonmail.ch>
2017-06-23 19:32:39 -0300

    Update comandos.py

--- comandos.py
+++ comandos.py
@@ -1,7 +1,6 @@
 import requests
 import emoji
 
-
 #função para extrair informações do enderesso de transação fornecido pelo User
 def transaction(hash, chat_id, bot):
      info_r = requests.get("https://api.blockcypher.com/v1/btc/main/txs/%s" %hash)
@@ -32,6 +31,24 @@ def wallet(hash, chat_id, bot):
     u_trans = int(info["unconfirmed_n_tx"])
     bot.sendMessage(chat_id, emoji.emojize("Endereço: %s \n\n:moneybag: Saldo: %f btc \n\n:money_with_wings: Total Recebido: %f btc \n\n:white_circle: Saldo não confirmado: %f btc \n\n"
                                            ":white_check_mark: Nº de transações confirmadas: %d \n\n:x: Transações não confirmadas: %d" %(hash, balance, t_received, un_balance, n_trans, u_trans), use_aliases=True))
+
+def fee(chat_id, bot, cmd):        #Mostra Fees recomendada
+    info_r = requests.get("https://bitcoinfees.21.co/api/v1/fees/recommended")
+    info = info_r.json()
+    fastest_fee = int(info["fastestFee"])
+    halfHour_fee = int(info["halfHourFee"])
+    hour_fee = int(info["hourFee"])
+    try:
+        out = int(cmd[1]) #números de endereços de entrada                     #Mostra fees recomendadas de acordo com sua transação estimando os bytes
+        inp = int(cmd[2])     #número de endereços de saída
+        size = out*34 + 180*inp + 10 #tamanho estimado da transação em Bytes
+        fastest_fee1 =  size * fastest_fee * 10**-8  #(Converte de satoshi para BTC)
+        halfHour_fee1 = size * halfHour_fee * 10**-8
+        hour_fee1 = size * hour_fee * 10**-8
+        bot.sendMessage(chat_id,emoji.emojize("Sua transação está estimada em %d bytes\n\n:rocket: Taxa rápida: %f btc \n\n:bullettrain_side: Até 30min para confirmar: %f btc\n\n:turtle: Até 1h para confirmar: %f btc" %(size, fastest_fee1, halfHour_fee1, hour_fee1), use_aliases=True))
+
+    except:
+        bot.sendMessage(chat_id, emoji.emojize(":clock1: Taxa mais rápida: \n%d sts/byte\n\n:clock130: Confirma em cerca de 30 min: %d sts/byte\n\n:clock2: Confirma em cerca de 1h: \n%d sts/byte"%(fastest_fee, halfHour_fee, hour_fee), use_aliases=True))
 
 #Função para enviar QR code em imagem para user
 def qr_code(hash, bot, chat_id):
