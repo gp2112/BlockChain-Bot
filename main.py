bd1527dffc0aef8d5b03fbfd8ebc61659e4c5866
gp2112 <guipaixao2000@protonmail.ch>
2017-06-23 19:29:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,34 @@ import telepot
 from comandos import *
 
 
-bot = telepot.Bot("")
+bot = telepot.Bot("TOKEN")
 
 print("Bot Iniciado!\n")
 
 
 def msg_tx(msg):
 
-    msg1 = str(msg["text"])     #texto enviado ao Bot
-    cmd = msg1.split()          #divide a string em lista
-    chat_id = msg["chat"]["id"] #Id do chat em List
-    chat_id_Str = str(chat_id)  #ID do chat em string
-    user = str(msg["from"]["username"])
-    print("Id: %s / User: @%s / Texto: %s" %(chat_id_Str, user, msg1))
+    try:
+        msg1 = str(msg["text"])     #texto enviado ao Bot
+        cmd = msg1.split()          #divide a string em lista
+        chat_id = msg["chat"]["id"] #Id do chat em List
+        chat_id_Str = str(chat_id)  #ID do chat em string
+        user = str(msg["from"]["username"])
+        print("Id: %s / User: @%s / Texto: %s" %(chat_id_Str, user, msg1)) #mandar logs através do console            
+        bot.sendMessage('''id do grupo privado''', "Id: %s\nUser: @%s\nTexto: %s" %(chat_id_Str, user, msg1)) #Função para mandar logs para mim através de um grupo privado no Telegram
+    except:
+        print("Erro de conexão")
 
     if msg["text"] == "/start":     #comando de iniciar Bot
         bot.sendMessage(chat_id, "Olá! Seja bem-vindo ao Block Chain Bot beta!\n Você pode consultar transações, carteiras e blocos utilizando comandos.\nDigite /help para ver os comandos.\n\nDesenvolvido por @A4narchy")
 
     elif msg["text"] == "/help": #comando para mostrar comandos disponíveis
         bot.sendMessage(chat_id, "Comandos:\n /t <hash da transação>: ver informações de uma transação"
-                                                    "\n /w <endereço da carteira>: ver informações de um endereço wallet"
-                                                    "\n /qr <endereço>: gera QR code para endereço")
+                                                    "\n\n /w <endereço da carteira>: ver informações de um endereço wallet"
+                                                    "\n\n /qr <endereço>: gera QR code para endereço"
+                                                    "\n\n /fee : veja as taxas de transação recomendadas ou use \n/fee <nº de endereços de saída> <nº de endereços de entrada> "
+                                                    "para estimar a quantidade de bytes em sua transação e calcuar uma taxa apropriada.\nExemplo: /fee 3 2"
+                                                    "\n\n /feedback <mensagem> : mande sugestões, elogios, informações de bugs e outros ao desenvolvedor")
     elif cmd[0] == "/t":
         try:
             hash = str(cmd[1])      #try para caso de comando errado
@@ -32,23 +39,36 @@ def msg_tx(msg):
             bot.sendMessage(chat_id, emoji.emojize(" :red_circle: Erro no comando! Digite /help para ver o modelo.", use_aliases=True))
 
 
-    elif cmd[0] == "/w":
+    elif cmd[0] == "/w":     #Envia informações da Wallet
         try:
             hash = str(cmd[1])
             wallet(hash, chat_id, bot)          #funções disponíveis no arquivo "comandos.py"
         except:
             bot.sendMessage(chat_id, emoji.emojize(" :red_circle: Erro no comando! Digite /help para ver o modelo.", use_aliases=True))
 
-    elif cmd[0] == "/qr":
+    elif cmd[0] == "/fee": #fee recomendada em satoshi/byte ou recomendada de acordo com o tamanho de byte da transação, em bitcoin
+        fee(chat_id, bot, cmd)
+
+
+    elif cmd[0] == "/size":
+        trx_size(chat_id, bot, cmd)
+
+    elif cmd[0] == "/qr":       #Envia o QR code da carteira
         try:
             hash = str(cmd[1])
             qr_code(hash, bot, chat_id)
         except:
             bot.sendMessage(chat_id, emoji.emojize(" :red_circle: Erro no comando! Digite /help para ver o modelo.", use_aliases=True))
 
+    elif cmd[0] == "/feedback": #Envia um Feedback
+        bot.sendMessage(chat_id, "Feedback enviado!\nObrigado pela colaboração!")
+        bot.sendMessage('''sua ID''', "Feedback de @%s: \"%s\"" %(user, msg["text"]))
+   #            sua ID para onde será enviado o Feedback
+
+
 bot.message_loop(msg_tx)
 
 while True:         #Looping
     pass
 
-    
+   
